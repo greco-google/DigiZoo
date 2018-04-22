6ed57b43450f3e80682d96e4a121faac5f34ce57
Josh Greco <greco.joshua@gmail.com>
2018-04-21 22:35:00 -0500

    Updating files

--- src/Pet.java
+++ src/Pet.java
@@ -8,6 +8,7 @@ public class Pet {
 	int thirst;
 	int bored;
 	int tired;
+	int waste;
 	
 
 
@@ -34,6 +35,7 @@ public class Pet {
 		thirst = thirst + (int) ((Math.random() * 5) + 1);
 		tired = tired + (int) ((Math.random() * 3) + 1);
 		health = health - (int) ((Math.random() * 3) + 1);
+		waste = waste + (int) ((Math.random() *2) + 1);
 	}
 	
 	public void walk() {
@@ -42,6 +44,7 @@ public class Pet {
 		thirst = thirst + (int) ((Math.random() * 5) + 1);
 		bored = bored - (int) ((Math.random() * 3) + 1);
 		health = health + (int) ((Math.random() * 3) + 1);
+		waste = 0;
 	}
 
 	
@@ -53,6 +56,57 @@ public class Pet {
 		health = health + (int) ((Math.random() * 3) + 1);
 	}
 	
+	public void vet() {
+		health = 100;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public int getHunger() {
+		return hunger;
+	}
+
+	public void setHunger(int hunger) {
+		this.hunger = hunger;
+	}
+
+	public int getThirst() {
+		return thirst;
+	}
+
+	public void setThirst(int thirst) {
+		this.thirst = thirst;
+	}
+
+	public int getBored() {
+		return bored;
+	}
+
+	public void setBored(int bored) {
+		this.bored = bored;
+	}
+
+	public int getTired() {
+		return tired;
+	}
+
+	public void setTired(int tired) {
+		this.tired = tired;
+	}
+
+	public int getWaste() {
+		return waste;
+	}
+
+	public void setWaste(int waste) {
+		this.waste = waste;
+	}
 	
 	
 //	@Override
