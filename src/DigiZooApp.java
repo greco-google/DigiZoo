0e3ac388c90336e076c577d8ce574e07b8f7afb2
Josh Greco <greco.joshua@gmail.com>
2018-05-01 23:09:57 -0500

    Making sure that everything is up to date

--- src/DigiZooApp.java
+++ src/DigiZooApp.java
@@ -23,41 +23,41 @@ public class DigiZooApp {
 		while (task != 9) {
 			if (task == 1) {
 				pet.feed();
-				buddy.tick();
+				pet.tick();
 				System.out.println("You are now feeding the animal...");
 			} else if (task == 2) {
-				buddy.drink();
-				buddy.tick();
+				pet.drink();
+				pet.tick();
 				System.out.println("The animal is drinking up...");
 			} else if (task == 3) {
-				buddy.play();
-				buddy.tick();
+				pet.play();
+				pet.tick();
 				System.out.println("You have chosen to play with the animal...");
 			} else if (task == 4) {
-				buddy.bathroom();
-				buddy.tick();
+				pet.bathroom();
+				pet.tick();
 				System.out.println("The animal is going to the bathroom...");
 			} else if (task == 5) {
-				buddy.sleep();
-				buddy.tick();
+				pet.sleep();
+				pet.tick();
 				System.out.println("The animal is now sleeping...");
 			} else if (task == 6) {
-				buddy.doc();
-				buddy.tick();
+				pet.doc();
+				pet.tick();
 				System.out.println("The animal is now going to go to the doctor...");
 			} else if (task == 7) {
-				buddy.nothing();
-				buddy.tick();
+				pet.nothing();
+				pet.tick();
 			}
 			else if (task == 8) {
-				System.out.println("Unfourtunatly you have entered a wrong number. Because of this, " + name + " got "
-						+ "very angry and ran away from you... \n\nOne year later...\n\n" + name + " has not been found and"
-								+ " you have given up hope on seeing your pet again :(. It is a very sad time. Please be "
+				System.out.println("Unfourtunatly you have entered a wrong number. Because of this, the animals got "
+						+ "very angry and ran away from the zoo... \n\nOne year later...\n\nThe animals have not been found and"
+								+ " you have given up hope on seeing your pets again :(. It is a very sad time. \nPlease be "
 								+ "more careful with your next pet.\n:):):) lol");
 				System.exit(0);
 			}
 
-			System.out.println(buddy);
+			System.out.println(pet);
 			System.out.println("\nWhat would you like to do with this animal?");
 			System.out.println("Press 1 to feed the animal ");
 			System.out.println("Press 2 to give the animal water");
@@ -66,7 +66,7 @@ public class DigiZooApp {
 			System.out.println("Press 5 to put the animal to sleep");
 			System.out.println("Press 6 to take the animal to to the doctor");
 			System.out.println("Press 7 to do nothing");
-			int task = input.nextInt();
+			int task1 = input.nextInt();
 			}
 	}
 
