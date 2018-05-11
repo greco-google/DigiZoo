523b6f39bc4a4800d900f2de7c45681acbddb8f7
Josh Greco <greco.joshua@gmail.com>
2018-05-11 17:10:33 -0500

    Minor Updates

--- src/Domesticated.java
+++ src/Domesticated.java
@@ -1,4 +1,13 @@
 
 public abstract class Domesticated extends Pet{
 
+    public void play() {
+        if (isSleeping) {
+            System.out.println("This pet is sleeping.");
+        } else {
+            boredom -= 20;
+            if (boredom < 0) boredom = 0;
+            System.out.println("Play done!");
+        }
+    }
 }
